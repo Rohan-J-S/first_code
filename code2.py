c31fc6679a25103cddd985d4d3c5a294c28095bc
Rohan-J-S <53927306+Rohan-J-S@users.noreply.github.com>
2019-08-09 10:29:57 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,2 +1,23 @@
-for x in range (10):
-  print (x)
+from threading import *
+from time import *
+class a(Thread):
+    def run(self):
+        for p in range (10):
+            print("a")
+            sleep(1)
+
+class b(Thread):   
+    def run(self):
+        for q in range (10):
+            print("b")
+            sleep(1)
+
+
+x = a()
+y = b()
+x.start()
+sleep(0.5)
+y.start()
+x.join()
+y.join()
+print("output was obtained")
